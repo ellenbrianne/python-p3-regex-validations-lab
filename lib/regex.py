58ae621a376b627ac118e09fa2d4a48fdd520ab7
Ellen Bennett <bennette831@gmail.com>
2023-10-09 03:36:45 -0600

    use re module, write regex patterns for name, phone_number, and email validations

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,10 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+name = r"^[A-Z]['|a-z][A-Z|a-z]+($|\s[A-Z][a-z]+[-|a-z][A-Z|a-z]+)"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"\W*[0-9]{3}\W*-?[0-9]{3}-?[0-9]{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"^[a-z]+\.*[a-z|0-9]+@[a-z]+\.[a-z]+"
 email_regex = re.compile(email_address)
